c43e29e30f5858e4feaadced168589c05cdfa557
humble <humble@nomail>
2016-10-20 03:40:05 +0500

    preview almost ok, feedback panels moved to separate tpl, enhanced - supports variables, images & right/left mode, preview image automatically cleared if image removed from fileupload panel

--- code/modules/default/default/default.php
+++ code/modules/default/default/default.php
@@ -4,10 +4,15 @@ require_once(PATH_LIB.'/db.lib.php');
 
 $tm = $DB->selectCell('SELECT NOW() AS tm');
 
+$li = file_get_contents(PATH_CTRL.'/lorem-ipsum.txt');
+
 return ctrlRetPrepare(
   [
     'data' => [
       'tm' => $tm,
+      'li' => $li,
+      'name' => 'iCoder XXI',
+      'email' => 'iCoder.XXI@gmail.com',
       'json' => [
         'message' => 'Hello World!',
       ],
